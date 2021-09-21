ab9fba92ee9e50c5b466bac73d9ce35b407938d9
nagolove <dekar.own@gmail.com>
2021-09-21 20:26:41 +0300

    mmap() works.

--- src/main.rs
+++ src/main.rs
@@ -20,22 +20,27 @@ extern "C" {
 const PROT_READ: i32 = 0x1;
 const PROT_WRITE: i32 = 0x2;
 const PROT_EXEC: i32 = 0x4;
+const MAP_ANONYMOUS: i32 = 0x20; /* Don't use a file.  */
+const MAP_PRIVATE: i32 = 0x02;  /* Changes are private.  */
 
 fn main() {
+
     unsafe {
         let addr: *mut c_void = ptr::null_mut();
         //let size = 1024 * 1024 * 1024 * 6;
         let size = 1024;
-        let prot = PROT_READ | PROT_WRITE | PROT_EXEC | 113;
+        let prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+        //let prot = PROT_EXEC;
         println!("errno {}", errno::errno());
-        let p: *mut i8 = mmap(addr, size, prot, 0, 0, 0) as *mut i8;
+        println!("addr {:?}", addr);
+        let p: *mut i8 = mmap(addr, size, prot, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0) as *mut i8;
         println!("errno {}", errno::errno());
         // хочу делать вот так
         //*p[1] = 0;
-        // непонимаю, что можно сделать со слайсом?
         let slice = slice::from_raw_parts_mut(p, 10);
         println!("mmap returned {:?}", p);
         slice[1] = 1;
+        println!("v {}", slice[1]);
         // Как обратиться к памяти выделенной на указатель p по индексу?
     }
 
@@ -68,10 +73,11 @@ fn main() {
      *}
      */
 
-    let address = 0x01230usize;
-    let r = address as *mut i8;
+    //let address = 0x01230usize;
+    //let r = address as *mut i8;
 
     //let slice: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
     //let slice: &mut [i8] = unsafe { slice::from_raw_parts_mut(r, 10000) };
     //slice[1] = 1;
+    //println!("{}", slice[1]);
 }
