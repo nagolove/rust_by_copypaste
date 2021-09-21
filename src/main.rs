182ded1049d9c05bdb1876db49a21d9b1fe4ed01
nagolove <dekar.own@gmail.com>
2021-09-21 20:07:22 +0300

    mmap() called with invalid argument.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 //#![allow(unused)]
 
+use errno;
 use core::ffi::c_void;
 use std::ptr;
 use std::slice;
 
 extern "C" {
-    fn mmap(addr: *mut c_void, size: usize, prot: i32, flags: i32, fildes: i32, off: i32) -> *mut c_void;
+    fn mmap(
+        addr: *mut c_void,
+        size: usize,
+        prot: i32,
+        flags: i32,
+        fildes: i32,
+        off: i32,
+    ) -> *mut c_void;
     //fn munmap();
 }
 
@@ -16,16 +24,54 @@ const PROT_EXEC: i32 = 0x4;
 fn main() {
     unsafe {
         let addr: *mut c_void = ptr::null_mut();
-        let size = 1024 * 1024 * 1024 * 6;
-        let prot = PROT_READ | PROT_WRITE | PROT_EXEC;
+        //let size = 1024 * 1024 * 1024 * 6;
+        let size = 1024;
+        let prot = PROT_READ | PROT_WRITE | PROT_EXEC | 113;
+        println!("errno {}", errno::errno());
         let p: *mut i8 = mmap(addr, size, prot, 0, 0, 0) as *mut i8;
+        println!("errno {}", errno::errno());
+        // хочу делать вот так
         //*p[1] = 0;
-        let slice = slice::from_raw_parts_mut(p as *mut i8, 1);
-        //println!("mmap returned {}", p.as_ptr());
+        // непонимаю, что можно сделать со слайсом?
+        let slice = slice::from_raw_parts_mut(p, 10);
+        println!("mmap returned {:?}", p);
+        slice[1] = 1;
         // Как обратиться к памяти выделенной на указатель p по индексу?
     }
 
     // wait for input
     let mut guess = String::new();
     std::io::stdin().read_line(&mut guess).expect("");
+
+    //let s: &str = "123";
+    //let ptr: *const u8 = s.as_ptr();
+
+    //unsafe {
+        //println!("{}", *ptr.offset(1) as char);
+        //println!("{}", *ptr.offset(2) as char);
+    //}
+
+    //let mut num = 5;
+    //println!("{}", num);
+    //let r1 = &num as *const i32;
+    ////unsafe { *r1 = 2; }
+    //println!("{}", num);
+    //let r2 = &mut num as *mut i32;
+    //unsafe { *r2 = 3; }
+    //println!("{}", num);
+
+    /*
+     *let address = 0x010395usize;
+     *let r = address as *mut i32;
+     *unsafe {
+     *    *r = 10;
+     *}
+     */
+
+    let address = 0x01230usize;
+    let r = address as *mut i8;
+
+    //let slice: &[i32] = unsafe { slice::from_raw_parts_mut(r, 10000) };
+    //let slice: &mut [i8] = unsafe { slice::from_raw_parts_mut(r, 10000) };
+    //slice[1] = 1;
 }
